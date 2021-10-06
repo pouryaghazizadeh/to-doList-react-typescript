c40c86961caea29f49130897e4e0375542767a63
pourya ghazizadeh <pourya.gh095@gmail.com>
2021-10-06 23:33:23 +0200

    make todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import InputField from "./components/form/index";
 import { Todo } from "./components/model/model";
 import { GlobalStyle } from "./GlobalStyle";
@@ -6,13 +6,24 @@ import { GlobalStyle } from "./GlobalStyle";
 const App:React.FC =()=> {
   const [todo,setTodo]=useState<string >("")
   const[todos,setTodos]=useState<Todo[ ]>([])
-  const handleAdd = ()=>{
+  const handleAdd = (e:React.FormEvent)=>{
+    // for see event type search it in google
+    e.preventDefault()
+    if(todo){
+      setTodos([...todos,{id:Date.now(),
+      todo,isDone:false
+     }])
+     setTodo("")
+     console.log(todos);
+ 
+    }
     
+
   }
   return (
     <div className="App">
       <span className="title">TASKIFY</span>
-      <InputField todo={todo} setTodo={setTodo}/>
+      <InputField todo={todo} setTodo={setTodo}  handleAdd={ handleAdd}/>
       <GlobalStyle/>
     </div>
   );
--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,18 +1,19 @@
 import {Form,Input,Button}from "./view"
 interface props{
     todo:string,
-    setTodo: React.Dispatch<React.SetStateAction<string>>
+    setTodo: React.Dispatch<React.SetStateAction<string>>,
+    handleAdd:(e:React.FormEvent)=>void
 }
-const InputField:React.FC<props> =({setTodo,todo})=> {
-    
+const InputField:React.FC<props> =({setTodo,todo,handleAdd})=> {
+
     return (
-        <Form>
+        <Form onSubmit={handleAdd}>
             <Input type="input" placeholder="Enter your task" value={todo}
             onChange={(e)=>{
                 setTodo(e.target.value)
             }}
             />
-            <Button type="submit">Go</Button>
+            <Button type="submit" >Go</Button>
         </Form>
     ) 
 }
